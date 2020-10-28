94482c50d2ada3a9e9c5d657a2fc6af8ef7575c1
aldiboss68 <alioune_diouf@live.fr>
2020-10-28 10:34:17 -0400

    Ajout de la fonction qui appelle de l'algo du code barre

--- imageupload_rest/viewsets.py
+++ imageupload_rest/viewsets.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from imageupload_rest.serializers import  UploadImageSerializer
 from imageupload.models import UploadImage
 from rest_framework.decorators import action
-from main import detect
+from main import detect,barcode
 from django.conf import settings
 import os
 
@@ -18,6 +18,16 @@ class UploadImageViewset(viewsets.ModelViewSet):
         if image is not None:
             val = detect.mainproc(os.path.join(settings.MEDIA_ROOT, image.image.name))
             return Response({'statut': 'success', 'ingredients': val[0],'valeurs nutritives': val[1]})
+        else:
+            return Response({'statut': 'echec'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
+    def reconBarcode(self, request, pk=None):
+        image = self.get_object()
+        if image is not None:
+            code_barre = barcode.get_string_barcode(os.path.join(settings.MEDIA_ROOT, image.image.name))
+            return Response({'statut': 'success', 'code_barre': code_barre[2]})
         else:
             return Response({'statut': 'echec'},
                             status=status.HTTP_400_BAD_REQUEST)
