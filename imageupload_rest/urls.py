e82247ccc02611f8acbdc5f60f25d4b69279fdcc
TheWiner1 <anoh.erwin05@gmail.com>
2020-10-28 23:33:47 -0400

    lien vers API

--- imageupload_rest/urls.py
+++ imageupload_rest/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from imageupload_rest.viewsets import UploadImageViewset
+
+app_name = "api_app"
+
 router = routers.DefaultRouter()
-router.register('images', UploadImageViewset, 'images')
+router.register('images', UploadImageViewset, basename="api_page")
 urlpatterns = [
     url('', include(router.urls))
 ]
