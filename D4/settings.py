f41dec13922b1fff0525b875008d2e49795cfc9c
TheWiner1 <anoh.erwin05@gmail.com>
2020-11-04 05:10:05 -0500

    fix static root again

--- D4/settings.py
+++ D4/settings.py
@@ -123,16 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'main', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'main', 'static')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
-)
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_media')
+
 # Base url to serve media files
 MEDIA_URL = '/media/'
 
